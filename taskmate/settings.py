e10df2394569990a7253291970273cb048841112
Anshuman7p <anshuman7p@gmail.com>
2024-05-30 17:01:51 +0530

    Domain Updates

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -21,7 +21,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate-project.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ['taskmate-project.onrender.com']
 
 
 # Application definition
