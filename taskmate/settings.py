7abd7673a0664958fa924cc25022c5209de0ddda
Anshuman7p <anshuman7p@gmail.com>
2024-05-31 10:59:37 +0530

    added the netlify

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -21,8 +21,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate7tart.netlify.app/"]
-CSRF_TRUSTED_ORIGINS = ['taskmate7tart.netlify.app/']
+ALLOWED_HOSTS = ["*"]
 
 
 
